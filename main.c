c932534e85b035ed791cc8cfaccc17646c3b6850
Arnaud <Arnaudltbr@gmail.com>
2023-04-25 21:14:24 +0200

    Pickpiece fonction

--- main.c
+++ main.c
@@ -1,248 +1,188 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 #include <time.h>
+
+
 typedef struct square {
-    int x;
-    int y;
-    int symbol;
+	int x;
+	int y;
+	int symbol;
 } Square;
 
 
 typedef struct piece {
-    Square squares[4];
+	Square squares[4];
 } Piece;
 
 
 typedef struct board {
-    Square grid[8][6];
+	Square grid[8][6];
 } Board;
 
+
 typedef struct player {
-    int PlayerNumber;
-    char name[20];
-    Piece ChosenPiece;
-    Board board;
-    int score;
-}Player;
+	int PlayerNumber;
+	char name[20];
+	Piece ChosenPiece;
+	Board board;
+	int score;
+} Player;
 
 
+// IDEA : return a piece instead of modifying the parameter
+void GeneratePiece(Piece* piece, int Pack) {
+    // Positions[Pack][Square][0 = x, 1 = y]
+    // Pack[] : 0 = I, 1 = L, 2 = O, 3 = T, 4 = S
+	int positions[5][4][2] = {
+	{{0,0}, {0,1}, {0,2}, {0,3}},
+	{{0,0}, {0,1}, {0,2}, {1,2}},
+	{{0,0}, {0,1}, {0,2}, {1,1}},
+	{{0,0}, {0,1}, {1,0}, {1,1}},
+	{{0,0}, {0,1}, {1,1}, {1,2}}
+	};
 
-void GenPiece(int Pack,Piece *piece){
-
-int randNum[4];
-int i;
-for (i=0;i<4;i++)
-randNum[i]=(rand()%5)+1;
-
-    switch(Pack){
-   case 1 :{
-        for (i=0;i<4;i++){
-            piece->squares[i].x=0;
-            piece->squares[i].y=i+1;
-            piece->squares[i].symbol=randNum[i];
-        }
-    break;
-    }
-
-
-    case 2 :{
-        for (i=0;i<3;i++){
-            piece->squares[i].x=1;
-            piece->squares[i].y=i+1;
-        }
-        piece->squares[3].x=2;
-        piece->squares[3].y=3;
-        for (i=0;i<4;i++)
-        piece->squares[i].symbol=randNum[i];
-        break;
-    }
-
-
-     case 3 :{
-        for (i=0;i<3;i++){
-            piece->squares[i].x=1;
-            piece->squares[i].y=i+1;
-        }
-        piece->squares[3].x=2;
-        piece->squares[3].y=2;
-        for (i=0;i<4;i++)
-        piece->squares[i].symbol=randNum[i];
-        break;
-    }
-
-
-     case 4 :{
-        for (i=0;i<2;i++){
-            piece->squares[i].x=1;
-            piece->squares[i].y=i+1;
-        }
-        for (i=2;i<4;i++){
-            piece->squares[i].x=2;
-            piece->squares[i].y=i-1;
-        }
-        for (i=0;i<4;i++)
-        piece->squares[i].symbol=randNum[i];
-        break;
-    }
-
-
-     case 5 :{
-        for (i=0;i<2;i++){
-            piece->squares[i].x=1;
-            piece->squares[i].y=i+1;
-        }
-        for (i=2;i<4;i++){
-            piece->squares[i].x=2;
-            piece->squares[i].y=i;
-        }
-        for (i=0;i<4;i++)
-        piece->squares[i].symbol=randNum[i];
-        break;
-    }
-
-
-     case 6 :{                                //1x1 square with a symbol, other squares are empty
-            piece->squares[0].x=1;
-            piece->squares[0].y=1;
-            piece->squares[0].symbol=randNum[1];
-
-            for (i=1;i<4;i++){
-                piece->squares[i].x=1;
-                piece->squares[i].y=i+2;
-                piece->squares[i].symbol=0;
-            }
-
-            break;
-    }
-}
-}
-;
-void PositionPiece(Piece *myPiece){ //Align piece on left and up
-//............................................................
-int i;
-// PLACER LA PIECE SUR LA GAUCHE
-    int test=myPiece->squares[0].x;
-    for(i=1;i<4;i++)
-    {
-        if (myPiece->squares[i].x<test)
-            test=myPiece->squares[i].x;
-    }
-    for(i=0;i<4;i++)
-    myPiece->squares[i].x-=(test);
-
-
-// PLACER LA PIECE SUR LE HAUT
-    test=myPiece->squares[0].y;
-    for(i=1;i<4;i++)
-    {
-        if (myPiece->squares[i].y<test)
-            test=myPiece->squares[i].y;
-    }
-    for(i=0;i<4;i++)
-    myPiece->squares[i].y-=(test);
-//............................................................
-
+	for (int i = 0; i < 4; i++) {
+		piece->squares[i].x = positions[Pack - 1][i][0];
+		piece->squares[i].y = positions[Pack - 1][i][1];
+		piece->squares[i].symbol = (rand() % 5) + 1;
+	}
 }
 
 
+void normalizePiece(Piece* myPiece) {
+	int i;
+	// Normalize the piece to the left
+	int test = myPiece->squares[0].x;
+	for (i = 1; i < 4; i++) {
+		if (myPiece->squares[i].x < test)
+			test = myPiece->squares[i].x;
+	}
+	for (i = 0; i < 4; i++)
+		myPiece->squares[i].x -= (test);
 
 
+	// Normalize the piece to the top
+	test = myPiece->squares[0].y;
+	for (i = 1; i < 4; i++) {
+		if (myPiece->squares[i].y < test)
+			test = myPiece->squares[i].y;
+	}
+	for (i = 0; i < 4; i++)
+		myPiece->squares[i].y -= (test);
+}
 
-void InitBoard(Board* board){
-    int i,j;
-    for(i=0;i<8;i++)
-        {
-            for(j=0;j<6;j++)
-            {
-                board->grid[i][j].x=i;
-                board->grid[i][j].y=j;
-                board->grid[i][j].symbol=0;
-            }
+
+void initEmptyBoard(Board* board) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 6; j++) {
+            board->grid[i][j].x = i;
+            board->grid[i][j].y = j;
+            board->grid[i][j].symbol = 0;
         }
-};
-
-void TestBoard(Board* board){
-int i,j;
-for(i=0;i<8;i++)
-    {
-        for(j=0;j<6;j++)
-        {
-            printf("%d",board->grid[i][j].x);
-            printf("%d",board->grid[i][j].y);
-            printf("%d",board->grid[i][j].symbol);
-            printf("   ");
-        }
-    printf("\n\n");
     }
-};
+}
+
+
+void testBoard(Board* board) {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 6; j++) {
+			printf("%d", board->grid[i][j].x);
+			printf("%d", board->grid[i][j].y);
+			printf("%d", board->grid[i][j].symbol);
+			printf("   ");
+		}
+		printf("\n\n");
+	}
+}
 
 
 void rotatePiece(Piece* piece) {
-    int centerX = 0;
-    int centerY = 0;
+	int centerX = 0;
+	int centerY = 0;
 
-    // Calculate the center of the piece
-    for (int i = 0; i < 4; i++) {
-        centerX += piece->squares[i].x;
-        centerY += piece->squares[i].y;
-    }
-    centerX = centerX/4;
-    centerY /= 4;
+	// Calculate the center of the piece
+	for (int i = 0; i < 4; i++) {
+		centerX += piece->squares[i].x;
+		centerY += piece->squares[i].y;
+	}
+	centerX = centerX / 4;
+	centerY /= 4;
 
-    // Rotate each square around the center
-    for (int i = 0; i < 4; i++) {
-        int relativeX = piece->squares[i].x - (centerX);
-        int relativeY = piece->squares[i].y + (centerY);
-        piece->squares[i].x = centerX + relativeY;
-        piece->squares[i].y = centerY - relativeX;
-            piece->squares[i].x-=1;
-            piece->squares[i].y+=1;
-    }
-    }
-
-void invertPiece(Piece* piece){
-for (int i = 0; i < 4; i++)
-piece->squares[i].x=4-piece->squares[i].x;
+	// Rotate each square around the center
+	for (int i = 0; i < 4; i++) {
+		int relativeX = piece->squares[i].x - (centerX);
+		int relativeY = piece->squares[i].y + (centerY);
+		piece->squares[i].x = centerX + relativeY;
+		piece->squares[i].y = centerY - relativeX;
+		piece->squares[i].x -= 1;
+		piece->squares[i].y += 1;
+	}
 }
 
 
-void printPiece(Piece piece) {
-    char grid[10][10];
+void invertPiece(Piece* piece) {
+	for (int i = 0; i < 4; i++)
+		piece->squares[i].x = 4 - piece->squares[i].x;
+}
 
-    // Initialize the grid with spaces
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            grid[i][j] =' ';
+
+void DEBUG_printPiece(Piece piece) {
+	char grid[10][10];
+
+	// Initialize the grid with spaces
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			grid[i][j] = ' ';
+		}
+	}
+
+	// Fill in the squares of the piece
+	for (int i = 0; i < 4; i++) {
+		int x = piece.squares[i].x;
+		int y = piece.squares[i].y;
+
+		if (piece.squares[i].symbol != 0)
+			grid[y][x] = 'X';
+	}
+
+	// Print the grid
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%c ", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void PickPieces(int RoundNumber, int NumberOfPlayers,Piece PickedPieces[]){
+    printf("\nChoose a bag between 1 and 6 : \n");
+    int bag;
+    scanf("%d",&bag);
+    printf("\n\n");
+    int i;
+    if (RoundNumber>1){
+        for (i=0;i<=NumberOfPlayers;i++){
+            GeneratePiece(&PickedPieces[i],bag);
         }
     }
-
-    // Fill in the squares of the piece
-    for (int i = 0; i < 4; i++) {
-        int x = piece.squares[i].x;
-        int y = piece.squares[i].y;
-        if (piece.squares[i].symbol!=0) // i c'est 0 la case est vide, sinon elle a un signe, on verra après pour les diférencier
-        grid[y][x] ='X';
-    }
-
-    // Print the grid
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("%c ", grid[i][j]);
+    else{
+        for (i=0;i<=NumberOfPlayers+1;i++){
+            GeneratePiece(&PickedPieces[i],bag);
         }
-        printf("\n");
     }
 }
 
 int main() {
-int choix;
-for(choix=1;choix<7;choix++){
-    Piece Mypiece;
-    GenPiece(choix,&Mypiece);
-    PositionPiece(&Mypiece);
-    printPiece(Mypiece);
-    printf("\n\n");
-    }
+int Roundnumber=2,NumberOfPlayers=4;
+Piece pickedpieces[NumberOfPlayers+1];
+PickPieces(Roundnumber,NumberOfPlayers,pickedpieces);
+int i;
+for (i=0;i<NumberOfPlayers+1;i++){
+    normalizePiece(&pickedpieces[i]);
+    DEBUG_printPiece(pickedpieces[i]);
+    printf("\n");
+}
 
     return 0;
 }
