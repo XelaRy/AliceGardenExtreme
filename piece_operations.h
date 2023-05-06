f903495c641d4e6de5125eaba2fabd359abdbfbd
XelaRy <Alexandroleboss@hotmail.fr>
2023-05-06 17:48:32 +0200

    Removed Deprecated Debug Function

--- piece_operations.h
+++ piece_operations.h
@@ -5,6 +5,5 @@ void generatePiece(Piece* piece, int Pack);
 void normalizePiece(Piece* myPiece);
 void rotatePiece(Piece* piece);
 void flipPiece(Piece* piece);
-void DEBUG_printPiece(Piece piece);
 
 #endif
