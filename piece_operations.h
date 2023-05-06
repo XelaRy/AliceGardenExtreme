3e5d381b1bfc17fe24cecc22ca308fc912f14745
XelaRy <Alexandroleboss@hotmail.fr>
2023-05-06 17:47:03 +0200

    Moved Piece Operations to Dedicated Modules

---
+++ piece_operations.h
@@ -0,0 +1,10 @@
+#ifndef piece_operations
+#define piece_operations
+
+void generatePiece(Piece* piece, int Pack);
+void normalizePiece(Piece* myPiece);
+void rotatePiece(Piece* piece);
+void flipPiece(Piece* piece);
+void DEBUG_printPiece(Piece piece);
+
+#endif
