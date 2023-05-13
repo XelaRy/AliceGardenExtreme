ffa45229294200e27df019f82154978645ea85a5
Arnaud <Arnaudltbr@gmail.com>
2023-05-13 18:52:50 +0200

    test1

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ void scoreCount(Player* player){
 //Count for Chess pieces
 for(i=0;i<6;i++){
     for(j=3;j<5;j++){
-        if (player->board[i][j]==4){
+        if (player->board[i][j]==5){
             player->score+=5;
         }    }
 }
@@ -29,7 +29,7 @@ int mushroomCount;
 for(j=0;j<8;j++){
     mushroomCount=0;
     for(i=0;i<6;i++){
-        if (player->board[i][j]==5){
+        if (player->board[i][j]==4){
             mushroomCount+=1;
         }    
     }
@@ -43,7 +43,7 @@ for(i=0;i<6;i++){
     firstTree=0;
     lastTree=0;
     for(j=0;j<8;j++){
-        if(player->board[i][j]==4){
+        if(player->board[i][j]==2){
             if(firstTree==0){
                 firstTree=j+1;
             }
