d0b0f7d1dd68d30aeb70c4f816313a302028ad0c
XelaRy <Alexandroleboss@hotmail.fr>
2023-05-13 14:57:46 +0200

    Optimized maxAdjacent and Added Multiple Comments

--- main.c
+++ main.c
@@ -41,18 +41,23 @@ void renderTextBox(SDL_Renderer* renderer, int windowWidth, int windowHeight, in
     SDL_DestroyTexture(textTexture);
 }
 
-
+// Recursive function to count the number of adjacent squares of the same symbol
+// Returns the number of adjacent squares
 int countAdjacentSquares(int grid[6][8], int sym, int x, int y) {
     int found = 1;
     int right = 0;
     int down = 0;
 
+    // If the square is not the same symbol or is already counted, return 0
     if (grid[y][x] != sym && grid[y][x] != -1) {
         return 0;
     }
+    // If the coordinates are on the edge of the grid, return 1
     if (x >= 8 || y >= 6) {
         return found;
     }
+    // Search for adjacent squares
+    // If the square is the same symbol, set it to -1 to avoid counting it again
     if (grid[y][x + 1] == sym) {
         right = 1;
         grid[y][x + 1] = -1;
@@ -62,9 +67,11 @@ int countAdjacentSquares(int grid[6][8], int sym, int x, int y) {
         grid[y + 1][x] = -1;
     }
 
+    // Return the number of adjacent squares and recursively call the function for each adjacent square
     return found + (right ? countAdjacentSquares(grid, sym, x + 1, y) : 0) + (down ? countAdjacentSquares(grid, sym, x, y + 1) : 0);
 }
 
+// Returns the maximum number of adjacent squares of the same symbol
 int maxAdjacentSymbols(int grid[6][8], int symbol) {
     int gridCopy[6][8];
     int max = 0;
@@ -76,7 +83,9 @@ int maxAdjacentSymbols(int grid[6][8], int symbol) {
 
     for (int i = 0; i < 6; i++)
         for (int j = 0; j < 8; j++) {
-            tmp = countAdjacentSquares(gridCopy, symbol, i, j);
+            // If the square is already counted, skip it
+            if (gridCopy[i][j] != -1)
+                tmp = countAdjacentSquares(gridCopy, symbol, i, j);
             if (tmp > max)
                 max = tmp;
         }
