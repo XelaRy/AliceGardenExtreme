13f31a6e779612c189cce4bab72f69540725337c
Arnaud <Arnaudltbr@gmail.com>
2023-05-13 10:33:40 +0200

    valid

--- main.c
+++ main.c
@@ -317,7 +317,7 @@ int main(int argc, char** argv) {
 
         // Play Button
         //renderTextBox(renderer, windowWidth, windowHeight, playButton.x, playButton.y, "Play", font, fontSize);
-        renderTextBox(renderer, windowWidth+10, windowHeight+10, playButton.x, playButton.y, "\n\tPlay\t\t\t", font, fontSize+50);
+        renderTextBox(renderer, windowWidth+90, windowHeight+90, playButton.x, playButton.y, "\n\tPlay\t\t\t", font, fontSize+50);
         
         SDL_RenderPresent(renderer);        
 
