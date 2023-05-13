3ce0bcb8abda3e2568e2a8acceb575454b79a74c
Arnaud <Arnaudltbr@gmail.com>
2023-05-13 15:18:32 +0200

    valid

--- main.c
+++ main.c
@@ -181,7 +181,7 @@ void endScreen(int windowWidth,int windowHeight,int name_length,char name[50],in
         renderTextBox(renderer, windowWidth, windowHeight, quitButton.x-200, quitButton.y-(400+(50*i)), text, font, fontSize-20);
     }
     // Quit Button
-    font = TTF_OpenFont("fonts/RobotoMono-Regular.ttf", fontSize);
+    font = TTF_OpenFont("fonts/RobotoMono-Regular.ttf", fontSize+2);
     renderTextBox(renderer, windowWidth, windowHeight, quitButton.x+100, quitButton.y,"QUIT GAME        " , font, fontSize);
     SDL_RenderPresent(renderer);        
     SDL_Delay(10);
